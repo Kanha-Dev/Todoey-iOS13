7ea016a3c9f6416b31ba3f5d630f97d89c9ce9df
Kanha Gupta <im.kanha2@gmail.com>
2024-07-13 13:43:25 +0530

    Add Checkmark to the todo list

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ToDoListViewController: UITableViewController {
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Larry", "Barry", "Garry"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - Table View Datasource Methods
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
