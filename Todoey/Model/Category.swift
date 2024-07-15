b2842f4f8986cc2465c7c6e37317eb916b1cacaa
Kanha Gupta <im.kanha2@gmail.com>
2024-07-15 15:16:21 +0530

    Implemented Realm instead of Core Data for Data Persistence

---
+++ Todoey/Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Kanha Gupta on 15/07/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kanha Gupta on 15/07/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
